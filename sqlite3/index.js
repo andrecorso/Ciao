633c2570c926ff59987bf1ddbeccb82afaa311cb
studente <studente@Licspalla.local>
2021-02-05 09:30:38 +0100

    a

--- sqlite3/index.js
+++ sqlite3/index.js
@@ -1,13 +1,14 @@
-const sqlite = require("sqlite3")
+const sqlite3 = require("sqlite3")
 
 const db  = new sqlite3.Database("./db.db3")
 
 db.serialize(() => {
-  db.run(`
-  CREATE TABLE IF NOT EXISTS Users (
-    username TEXT,
-    password TEXT,
-  )`)
+	db.run(`
+	CREATE TABLE IF NOT EXISTS Users (
+	  username TEXT NOT NULL,
+	  password TEXT NOT NULL,
+	  UNIQUE(username, password)
+	)`)
 })
 
 let query = "INSERT INTO Users VALUES"
@@ -24,12 +25,13 @@ app.use(express.json())
 
 app.post("/login", (req, res) => {
 	const {username, password} = req.body
+	console.log({username, password})
 	if(username && password) {
-		db.all("SELECT * FROM Users WHERE username = ? AND password = ?", user, password, (err, rows) => {
+		db.all("SELECT * FROM Users WHERE username = ? AND password = ?", username, password, (err, rows) => {
 		    if (err) {
 		        res.status(500).send(err.message)
 		    } else if (rows.length) {
-		        res.status(200).send({ ok: true, jwt: signToken(user) })
+		        res.status(200).send({ ok: true })
 		    } else {
 		        res.status(401).send({ ok: false })
 		    }
